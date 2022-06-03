9c479f5731277c774de16479d32ea49c8ce9aa2b
DennisArgemiro <dennisarg2011@gmail.com>
2022-06-03 17:03:37 -0300

    Att dos Botoes superiores

---
+++ scripts/routine.js
@@ -0,0 +1,18 @@
+import {db} from "../scripts/database.js"
+
+const nomeRotina = document.querySelector("#nameRot");
+
+nomeRotina.textContent = localStorage.getItem('RotKey')
+
+const infoBtn = document.querySelector("#infoBtn")
+
+infoBtn.addEventListener("click", ()=>{
+    db.transaction(function(tx){
+        tx.executeSql(`SELECT * FROM routines WHERE nome = '${localStorage.getItem('RotKey')}'`,[],function(tx, result){
+            const rows = result.rows
+
+            alert(rows[0].desc)
+        }, null)
+})
+})
+

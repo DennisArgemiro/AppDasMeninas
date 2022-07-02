3c706a1be08fc09fd48d1176c748edf5aee91425
Dennis Argemiro <88357881+DennisArgemiro@users.noreply.github.com>
2022-07-01 23:50:06 -0300

    Update database.js

--- scripts/database.js
+++ scripts/database.js
@@ -1,11 +1,11 @@
 //banco de dados
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
-const email = "dennisarg2011@gmail.com";
-const password  = "livec4f3"
+const email = "";
+const password  = ""
 
-const urlKey = "https://ziaodzioajpxysrjbhzs.supabase.co"
-const apiKey =  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InppYW9kemlvYWpweHlzcmpiaHpzIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTM4MjgzNjAsImV4cCI6MTk2OTQwNDM2MH0.rWHdfQ9sg80dvoszqmbzDQbpuBxq3LgEMu9Zx0Wzq5Q"
+const urlKey = ""
+const apiKey =  ""
 
 export const supabase = createClient(
   urlKey,
